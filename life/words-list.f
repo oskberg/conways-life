b37a495767ff35d585a8e0e8db0c00d23928bca4
Luke-10100 <77681899+Luke-10100@users.noreply.github.com>
2021-01-21 11:20:43 +0000

    Added rule check to words-list.

--- life/words-list.f
+++ life/words-list.f
@@ -66,3 +66,17 @@ LOOP ;
 0 DO
     2 rnd over I + ! 
 LOOP ;
+
+{ returns 1 if alive and 0 if dead }
+: LIFE-RULE ( status neighbours -- )
+    CASE 
+        0 OF drop 0 ENDOF 
+        1 OF drop 0 ENDOF 
+        2 OF 1 = abs ENDOF   { if current status = 0 return 0, else abs( -1 ). Could also do nothing }
+        3 OF drop 1 ENDOF 
+        4 OF drop 0 ENDOF 
+        5 OF drop 0 ENDOF 
+        6 OF drop 0 ENDOF 
+        7 OF drop 0 ENDOF 
+        8 OF drop 0 ENDOF 
+    ENDCASE ;
